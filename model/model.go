69e2643a0b88e6ee33bbb1c21c37b372db977b1e
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-01-08 11:43:33 +0530

    functions done

--- model/model.go
+++ model/model.go
@@ -1,5 +1,14 @@
 package model
 
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
 type Goly struct {
 	ID       uint64 `json:"id" gorm:"primaryKey"`
 	Redirect string `json:"redirect"`
@@ -8,6 +17,16 @@ type Goly struct {
 	Random   bool   `json:"random"`
 }
 
-func Setup(){
+func Setup() {
 	dsn := "host=127.0.0.1 user=postgres password=123 dbname=go-bitly-clone port=5432"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.AutoMigrate(&Goly{})
+	if err != nil {
+		fmt.Println(err)
+	}
 }
---
+++ utils/random.go
@@ -0,0 +1,16 @@
+package utils
+
+import "math/rand"
+
+var runes = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandomUrl(size int) string {
+	str := make([]rune, size)
+
+	for i := range str {
+		str[i] = runes[rand.Intn(len(runes))]
+	}
+
+	return string(str)
+
+}
