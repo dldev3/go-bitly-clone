eac6d06f3ff966a2afc186d9b5ead535cb0f30ec
dlshnkLK <dilshankaliyanage@yahoo.com>
2023-01-09 19:03:00 +0530

    finished

--- view/src/App.svelte
+++ view/src/App.svelte
@@ -1,11 +1,14 @@
 <script>
 	import List from "./components/List.svelte";
+	import New from "./components/New.svelte";
 </script>
 
 <main>
 	<h1>Goly -- The Go URL Shortner</h1>
 	<List />
 </main>
+
+<New />
 
 <style>
 </style>
--- view/src/components/New.svelte
+++ view/src/components/New.svelte
@@ -1,3 +1,47 @@
 <script>
-    
+    import { Modals, closeModal, openModal } from "svelte-modals";
+    import Modal from "./Modal.svelte";
+
+    async function createGoly(data) {
+        const json = {
+            redirect: data.redirect,
+            goly: data.goly,
+            random: data.random,
+        };
+
+        await fetch("http://localhost:3000/goly", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(json),
+        }).then((res) => {
+            console.log(res);
+        });
+    }
+
+    function handleOpen() {
+        openModal(Modal, {
+            title: "Create New Goly Link",
+            send: createGoly,
+            redirect: "",
+            goly: "",
+            random: false,
+        });
+    }
 </script>
+
+<button on:click={handleOpen}>Create a New Goly Link</button>
+
+<style>
+    button {
+        background-color: green;
+        color: white;
+        font-weight: bold;
+        border: none;
+        padding: 0.5rem;
+        border-radius: 0.25rem;
+    }
+
+    button:hover {
+        cursor: pointer;
+    }
+</style>
